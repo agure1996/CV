c56e5bee205a128660251c95d5b359ec208ba9c8
160197415 <72154777+160197415@users.noreply.github.com>
2021-06-10 22:03:45 +0100

    Added files
    Java file
    html
    css
    js files

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    static Integer c= 2;
+    static int a = 2;
+    static String b = "abbas";
+    public static void main(String[] args) throws Exception {
+        System.out.println(a+c);
+    }
+}
---
+++ myscript.js
@@ -0,0 +1,7 @@
+
+var username = "abbas";
+var greeting = "Hello " + username;
+document.querySelector('p').onclick = alert(greeting);
+var ul = document.createElement('ul');
+document.getElementById("text-area").appendChild(ul);
+
